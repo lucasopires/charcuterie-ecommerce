783b1da9c1a0a8cf5786e1900ef33e5ba7c070a3
Lucas Pires <lucas@Angelas-MacBook-Pro.local>
2021-03-12 20:05:04 -0500

    initial commit

---
+++ sc-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+// import { AbsoluteSourceSpan } from '@angular/compiler';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
