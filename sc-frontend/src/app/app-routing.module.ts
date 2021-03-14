4d85a8b04a72478cf3494cc48b1fa6ae905cef8f
Lucas Pires <lucas@Angelas-MacBook-Pro.local>
2021-03-14 15:55:15 -0400

    implementing home products and routes

--- sc-frontend/src/app/app-routing.module.ts
+++ sc-frontend/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
