b927087afbac6e965f4570829d3fcce3609ac575
Lucas Pires <lucas@Angelas-MacBook-Pro.local>
2021-03-14 20:01:50 -0400

    including sc-backend

---
+++ sc-backend/src/test/java/ca/angelastable/ScBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.angelastable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
